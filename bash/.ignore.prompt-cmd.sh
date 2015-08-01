1bef04c0542af8f00051b60aaf59fd26136d2754
Lucas Hart <lukke100@gmail.com>
2015-08-01 13:09:49 -0400

    Remove Duplicate Slash in Root

--- bash/.ignore.prompt-cmd.sh
+++ bash/.ignore.prompt-cmd.sh
@@ -1,2 +1,4 @@
-PS1="$(PUT_HEADER)\n"$'\E[48;5;15;38;5;0m'"\w/  λ "$'\E[0;38;5;15m\uE0B0\E[0m ';
+PROMPT_TRAIL="/"
+[[ $PWD != "/" ]] || PROMPT_TRAIL=;
+PS1="$(PUT_HEADER)\n"$'\E[48;5;15;38;5;0m'"\w$PROMPT_TRAIL  λ "$'\E[0;38;5;15m\uE0B0\E[0m ';
 PUT_TITLE;
