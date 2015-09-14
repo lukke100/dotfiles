cd782f926befc837be21898b7cb3694fafe88051
Lucas Hart <lukke100@gmail.com>
2015-09-13 22:35:31 -0400

    Remove ANSI Escapes from Bash Prompt

--- bash/.ignore.prompt-cmd.sh
+++ bash/.ignore.prompt-cmd.sh
@@ -1,4 +1,4 @@
 PROMPT_TRAIL="/";
 [[ $PWD != "/" ]] || PROMPT_TRAIL=;
-PS1="$(PUT_HEADER)\n"$'\E[48;5;15;38;5;0m'"\w$PROMPT_TRAIL  λ "$'\E[0;38;5;15m\uE0B0\E[0m ';
+PS1="$(PUT_HEADER)\n\w$PROMPT_TRAIL λ -> ";
 PUT_TITLE;
