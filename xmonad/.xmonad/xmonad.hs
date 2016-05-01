78b0d5619228ed94e02db35977e6db141cfcfd0d
Lucas Hart <lukke100@gmail.com>
2016-05-01 09:07:54 -0400

    change formatting for xmonad code

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,11 +1,11 @@
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.GridVariants
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
-import XMonad.Layout.NoBorders
-import XMonad.Layout.GridVariants
-import XMonad.Actions.CycleWS
 
 brSelBg, brSelTx, brTtlTx, bdFocus, bdNorm :: String
 brSelBg = "#8667e0"
@@ -18,46 +18,43 @@ barPath :: String
 barPath = "xmobar ~/.xmonad/xmobar.rc"
 
 tLayout = Tall 1 (3/100) (1/2)
-layouts =   spacing 24 (Grid (16/10))
-        ||| spacing 24 tLayout
-        ||| spacing 24 (Mirror tLayout)
-        ||| Full
+layouts = spacing 24 (Grid (16/10))
+      ||| spacing 24 tLayout
+      ||| spacing 24 (Mirror tLayout)
+      ||| Full
 
 main :: IO ()
 main = statusBar barPath barFmt toggleStrutsKey conf >>= xmonad
 
 barFmt :: PP
 barFmt = xmobarPP
-    { ppCurrent   = xmobarColor brSelTx brSelBg . wrap " λ " " "
-    , ppTitle     = xmobarColor brTtlTx ""
-    , ppVisible   = wrap "[" "]"
-    , ppUrgent    = xmobarColor "red" "yellow"
-    }
+  { ppCurrent = xmobarColor brSelTx brSelBg . wrap " λ " " "
+  , ppTitle   = xmobarColor brTtlTx ""
+  , ppVisible = wrap "[" "]"
+  , ppUrgent  = xmobarColor "red" "yellow"
+  }
 
 toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig {XMonad.modMask = m} = (m, xK_b)
+toggleStrutsKey XConfig { XMonad.modMask = m } = (m, xK_b)
 
 conf = ewmh $ defaultConfig
-    { terminal              = "urxvt"
-    , focusFollowsMouse     = False
-    , layoutHook            = smartBorders layouts
-    , focusedBorderColor    = bdFocus
-    , normalBorderColor     = bdNorm
-    , borderWidth           = 2
-    , workspaces            = ["term", "code", "http", "media", "misc"]
-    , handleEventHook       = handleEventHook defaultConfig <+> fullscreenEventHook
-    , clickJustFocuses      = False
-    }
-    `additionalKeys`
-        -- [ ((mod4Mask,                   xK_l),          spawn "xscreensaver-command -lock")
-        -- , ((mod1Mask,                   xK_Down),       nextWS)
-        [ ((mod1Mask,                   xK_Down),       nextWS)
-        , ((mod1Mask,                   xK_Up),         prevWS)
-        , ((mod1Mask .|. shiftMask,     xK_Down),       shiftToNext)
-        , ((mod1Mask .|. shiftMask,     xK_Up),         shiftToPrev)
-        , ((mod1Mask,                   xK_Right),      nextScreen)
-        , ((mod1Mask,                   xK_Left),       prevScreen)
-        , ((mod1Mask .|. shiftMask,     xK_Right),      shiftNextScreen)
-        , ((mod1Mask .|. shiftMask,     xK_Left),       shiftPrevScreen)
-        , ((mod1Mask,                   xK_p),          spawn "rofi -show run")
-        ]
+  { terminal            = "urxvt"
+  , focusFollowsMouse   = False
+  , layoutHook          = smartBorders layouts
+  , focusedBorderColor  = bdFocus
+  , normalBorderColor   = bdNorm
+  , borderWidth         = 2
+  , workspaces          = ["term", "code", "http", "media", "misc"]
+  , handleEventHook     = handleEventHook defaultConfig <+> fullscreenEventHook
+  , clickJustFocuses    = False
+  } `additionalKeys`
+    [ ((mod1Mask,               xK_Down),   nextWS)
+    , ((mod1Mask,               xK_Up),     prevWS)
+    , ((mod1Mask .|. shiftMask, xK_Down),   shiftToNext)
+    , ((mod1Mask .|. shiftMask, xK_Up),     shiftToPrev)
+    , ((mod1Mask,               xK_Right),  nextScreen)
+    , ((mod1Mask,               xK_Left),   prevScreen)
+    , ((mod1Mask .|. shiftMask, xK_Right),  shiftNextScreen)
+    , ((mod1Mask .|. shiftMask, xK_Left),   shiftPrevScreen)
+    , ((mod1Mask,               xK_p),      spawn "rofi -show run")
+    ]
