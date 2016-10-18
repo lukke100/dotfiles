e28ed639582ac7dcb8572fcb8209c00a6f32becb
Lucas Hart <lukke100@gmail.com>
2016-10-17 22:10:04 -0400

    Build `xmonad/.xmonad/xmonad.hs` in `link-all.sh`
      * the `stack` command will pull all dependencies as needed
      * change `pwd` of `link-all.sh` for proper `stow` behavior

--- link-all.sh
+++ link-all.sh
@@ -2,8 +2,27 @@
 set -euo pipefail
 IFS=$(printf '\n\t')
 
+SCRIPT_SRC=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_SRC")
+
+cd "$SCRIPT_DIR"
+
+unset SCRIPT_SRC
+unset SCRIPT_DIR
+
 # shellcheck disable=SC2035
 stow --no-folding "$@" -vt ~ */ --ignore='\.ignore\.[^/]*'
+
+XMONAD_DIR=~/.xmonad
+XMONAD_SRC=$XMONAD_DIR/xmonad.hs
+
+if [ -f "$XMONAD_SRC" ] && [ -r "$XMONAD_SRC" ]
+then
+  (set -x; stack ghc -- "$XMONAD_SRC" -o "$XMONAD_DIR/xmonad-x86_64-linux")
+fi
+
+unset XMONAD_DIR
+unset XMONAD_SRC
 
 systemctl --user enable mpd.service
 systemctl --user enable dropbox.service
