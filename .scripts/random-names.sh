ddb7d2d3407a857c8da084ce18ae9471a7517ee5
Lucas Hart <lukke100@gmail.com>
2017-02-11 22:58:36 -0500

    Add script that randomizes filenames
      * filenames in dotfiles may otherwise contain sensitive information

---
+++ .scripts/random-names.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+set -euo pipefail
+IFS=$(printf '\n\t')
+
+IGNORE_LEN=12
+
+for IMPORT_PATH in "$@"
+do
+  IMPORT_FLDR=$(dirname "$IMPORT_PATH")
+  IMPORT_NAME="${IMPORT_PATH##*/}"
+  IMPORT_SUFX="${IMPORT_NAME##*.}"
+  IMPORT_NAME="${IMPORT_NAME%.*}"
+  EXPORT_NAME="$(pwgen 12 1)"
+
+  if [ "$IMPORT_NAME" = "$IMPORT_SUFX" ]
+  then
+    IMPORT_SUFX=""
+  else
+    IMPORT_SUFX=".$IMPORT_SUFX"
+  fi
+
+  EXPORT_PATH="$IMPORT_FLDR/$EXPORT_NAME$IMPORT_SUFX"
+
+  if [ "${#IMPORT_NAME}" -ne "$IGNORE_LEN" ]
+  then
+    mv --verbose "$IMPORT_PATH" "$EXPORT_PATH"
+  fi
+
+  unset EXPORT_PATH
+  unset EXPORT_NAME
+  unset IMPORT_SUFX
+  unset IMPORT_NAME
+  unset IMPORT_FLDR
+done
+
+unset IGNORE_LEN
