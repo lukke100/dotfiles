5e0498be6f2e1c8028823caeae74e3012ffacdde
Lucas Hart <lukke100@gmail.com>
2017-09-17 11:09:09 -0400

    Remove Xmonad code within "link-all.sh"
    I plan to change away from Xmonad at some point.

--- link-all.sh
+++ link-all.sh
@@ -10,14 +10,6 @@ cd "$SCRIPT_DIR"
 # shellcheck disable=SC2035
 stow --no-folding "$@" -vt ~ */ --ignore='\.ignore\.[^/]*'
 
-XMONAD_DIR=~/.xmonad
-XMONAD_SRC=$XMONAD_DIR/xmonad.hs
-
-if [ -f "$XMONAD_SRC" ] && [ -r "$XMONAD_SRC" ]
-then
-  (set -x; stack ghc -- "$XMONAD_SRC" -o "$XMONAD_DIR/xmonad-x86_64-linux")
-fi
-
 systemctl --user enable mpd.service
 systemctl --user enable dropbox.service
 
